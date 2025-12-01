e20126472b9525e2f02b4b3801c01f428c3f9b97
Gmbrandk <gmbrandk@gmail.com>
2025-12-01 11:24:41 -0500

    Preparacion de archivos de configuracion y dependencias para instalacion de nuevo modulo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
